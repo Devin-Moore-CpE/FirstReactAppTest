7d6b920235a50666d8b1d63d30503b850038b309
Devin Moore <devinmoore89@gmail.com>
2024-12-01 21:12:51 -0800

    Edited the text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello. Cool logo, huh?
         </p>
         <a
           className="App-link"
